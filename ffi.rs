0c8ad1797e4b105c4a01166ed076ec939624b7d4
Tomoki Aonuma <uasi@uasi.jp>
2014-10-25 21:56:34 +0900

    Add ffi.rs

---
+++ ffi.rs
@@ -0,0 +1,29 @@
+extern crate libc;
+
+use libc::{c_ulong, c_uint};
+use std::ptr;
+
+mod z {
+    use libc::{c_ulong, c_uint};
+
+    #[link(name = "z")]
+    extern {
+        pub fn adler32(adler: c_ulong, buf: *const u8, len: c_uint) -> c_ulong;
+    }
+}
+
+fn adler32(adler: u64, buf: &[u8]) -> u64 {
+    unsafe {
+        match buf.len() {
+            0 => z::adler32(adler as c_ulong, ptr::null(), 0 as c_uint) as u64,
+            _ => z::adler32(adler as c_ulong, buf.as_ptr(), buf.len() as c_uint) as u64
+        }
+    }
+}
+
+fn main() {
+    let adler = adler32(0, &[]);
+    println!("initial value: {}", adler);
+    let adler = adler32(0, &[1u8, 2u8, 3u8]);
+    println!("put [1, 2, 3]: {}", adler);
+}
