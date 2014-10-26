b0fc146d0e3523cca101aec1e47e5cd1285666ab
Tomoki Aonuma <uasi@uasi.jp>
2014-10-26 09:53:57 +0900

    Update libxml2.rs

--- libxml2.rs
+++ libxml2.rs
@@ -1,25 +1,90 @@
+extern crate core;
 extern crate libc;
 
-use libc::{c_char, c_int, c_void};
+use core::raw::Slice;
+use libc::{c_char, c_uchar, c_ushort, c_int, c_void};
+use std::mem;
 use std::ptr;
 
 static HTML_PARSE_NOERROR: c_int = 32;
 static HTML_PARSE_NOWARNING: c_int = 64;
 
+#[allow(non_camel_case_types)]
+type xmlChar = c_uchar;
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+struct xmlDoc {
+    _private: *mut c_void,
+    type_: c_int, // xmlElementType
+    name: *const xmlChar,
+    children: *mut xmlNode,
+    last: *mut xmlNode,
+    parent: *mut xmlNode,
+    next: *mut xmlNode,
+    prev: *mut xmlNode,
+    doc: *mut xmlDoc,
+    compression: c_int,
+    standalone: c_int,
+    intSubset: *mut c_void, // *xmlDtd
+    extSubset: *mut c_void,
+    oldNs: *mut c_void, // *xmlNs
+    version: *const xmlChar,
+    encoding: *const xmlChar,
+    ids: *mut c_void,
+    refs: *mut c_void,
+    URL: *const xmlChar,
+    charest: c_int,
+    dict: *mut c_void, // *xmlDict
+    psvi: *mut c_void,
+    parseFlags: c_int,
+    properties: c_int
+}
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+struct xmlNode {
+    _private: *mut c_void,
+    type_: c_int, // xmlElementType
+    name: *const xmlChar,
+    children: *mut xmlNode,
+    last: *mut xmlNode,
+    parent: *mut xmlNode,
+    next: *mut xmlNode,
+    prev: *mut xmlNode,
+    doc: *mut xmlDoc,
+    ns: *mut c_void, // *xmlNs
+    content: *mut xmlChar, // *xmlChar
+    properties: *mut c_void, // *xmlAttr
+    nsDef: *mut c_void, // *xmlNs
+    psvi: *mut c_void,
+    line: c_ushort,
+    extra: c_ushort
+}
+
 #[link(name = "xml2")]
 #[allow(non_camel_case_types)]
 extern {
-    fn htmlReadMemory(buffer: *const c_char, size: c_int, url: *const c_char, encoding: *const c_char, options: c_int) -> *mut c_void;
-    fn xmlFreeDoc(cur: *mut c_void);
+    fn htmlReadMemory(buffer: *const c_char, size: c_int, url: *const c_char, encoding: *const c_char, options: c_int) -> *mut xmlDoc;
+    fn xmlDocGetRootElement(doc: *const xmlDoc) -> *mut xmlNode;
+    fn xmlFreeDoc(cur: *mut xmlDoc);
+    fn xmlFreeNode(cur: *mut xmlNode);
+    fn xmlStrlen(str: *const xmlChar) -> c_int;
+}
+
+unsafe fn xml_str_to_slice<'a>(xml_str: *const xmlChar) -> &'a str {
+    mem::transmute(Slice { data: xml_str, len: xmlStrlen(xml_str) as uint })
 }
 
 struct XmlDoc {
-    doc_ptr: *mut c_void,
+    ptr: *mut xmlDoc
 }
 
 impl Drop for XmlDoc {
     fn drop(&mut self) {
-        unsafe { xmlFreeDoc(self.doc_ptr); }
+        unsafe { xmlFreeDoc(self.ptr); }
     }
 }
 
@@ -32,13 +97,42 @@ impl XmlDoc {
             if doc_ptr.is_null() {
                 None
             } else {
-                Some(XmlDoc { doc_ptr: doc_ptr })
+                Some(XmlDoc { ptr: doc_ptr })
+            }
+        }
+    }
+
+    // Dangerous!
+    // If the returned node outlives the doc, the node's ptr will be freed!
+    fn root_element(&self) -> XmlNode {
+        unsafe {
+            XmlNode { ptr: xmlDocGetRootElement(mem::transmute(self.ptr)) }
+        }
+    }
+}
+
+struct XmlNode {
+    ptr: *mut xmlNode
+}
+
+impl Drop for XmlNode {
+    fn drop(&mut self) {
+        unsafe {
+            if (*self.ptr).parent.is_null() {
+               xmlFreeNode(self.ptr);
             }
         }
     }
 }
 
+impl XmlNode {
+    fn name(&self) -> &str {
+        unsafe { xml_str_to_slice((*self.ptr).name) }
+    }
+}
+
 fn main() {
-    let doc = XmlDoc::from_html_str("<html></html>");
+    let doc = XmlDoc::from_html_str("<html><body>BODY</body></html>");
     assert!(doc.is_some());
+    println!("root element name: {}", doc.unwrap().root_element().name());
 }
