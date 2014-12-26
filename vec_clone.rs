fbf002656807cf7bc5129cd8c502e03d6f1e6000
Tomoki Aonuma <uasi@uasi.jp>
2014-12-26 16:58:20 +0900

    Add vec_clone.rs

---
+++ vec_clone.rs
@@ -0,0 +1,20 @@
+#[deriving(Show)]
+struct Noncloneable;
+
+#[deriving(Show, Clone)]
+struct Cloneable;
+
+fn main() {
+    let vn: Vec<Noncloneable> = Vec::new();
+    let _: &[Noncloneable] = vn.clone();
+    //                       ^^^^^^^^^^
+    //                       vn auto-derefs to &[Noncloneable], which impls Clone,
+    //                       so this expr returns a cloned &[Nonclonable].
+
+    let vc: Vec<Cloneable> = Vec::new();
+    let _: Vec<Cloneable> = vc.clone();
+    //                      ^^^^^^^^^^
+    //                      Vec<Clonable> itself impls Clone,
+    //                      so this expr returns a cloned Vec<Cloneable>.
+}
+
